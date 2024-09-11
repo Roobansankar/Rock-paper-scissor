82a7f9a2891b47fe0bc4494d4a4b10408ed250e3
Roobansankar-settyl <rooban.sankar@settyl.com>
2024-09-11 17:09:27 +0530

    update s

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ app.use(express.json());
 // MongoDB Connection Handling
 let cachedDb = null;
 async function connectToDatabase(uri) {
-  if (cachedDb) return cachedDb; // Use cached connection if available
+  if (cachedDb) return cachedDb; 
   const db = await mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+  }); 
   cachedDb = db;
   console.log("Connected to MongoDB");
   return cachedDb;
