d400256642d1344f71502965ed836054acfa334f
Roobansankar-settyl <rooban.sankar@settyl.com>
2024-09-11 16:58:13 +0530

    server update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const app = express();
+
+// CORS options
 const corsOptions = {
   origin: process.env.APPLICATION_URL,
   methods: "GET,HEAD,PUT,PATCH",
@@ -15,19 +17,36 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 
+// Cached database connection to improve performance
+let cachedDb = null;
+
+async function connectToDatabase(uri) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  // Establish MongoDB connection
+  const db = await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = db;
+  console.log("Connected to MongoDB...");
+  return cachedDb;
+}
+
+// DB Connection Initialization
+connectToDatabase(process.env.MONGODB_URL).catch((err) =>
+  console.error("Error connecting to MongoDB:", err)
+);
+
 // Router
 const infoRouter = require("./router");
-const { method } = require("lodash");
 app.use("/info", infoRouter);
 
-// listen port
+// Listen on the specified port or default to 5013
 const PORT = process.env.PORT || 5013;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
-// Db Connection
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("Connected To MongoDB..."))
-  .catch((err) => console.log(err));
--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const InfoRouter = require("./infoSchema");
 
-// Create
+// Create new data
 router.post("/", async (req, res) => {
   try {
     const data = new InfoRouter({
@@ -12,18 +12,18 @@ router.post("/", async (req, res) => {
       User2Result: req.body.User2Result,
     });
     await data.save();
-    res.json(data);
+    res.status(201).json(data); // Return status 201 for creation
   } catch (error) {
     console.error("Error creating data:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// Get all
+// Get all data
 router.get("/", async (req, res) => {
   try {
     const findData = await InfoRouter.find();
-    res.json(findData);
+    res.status(200).json(findData);
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "Internal Server Error" });
