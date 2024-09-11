aceca100a69ba9655eb235d4e6b7b04f45300b34
Roobansankar-settyl <rooban.sankar@settyl.com>
2024-09-11 14:39:38 +0530

    chan

--- server/index.js
+++ server/index.js
@@ -1,39 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose');
-// const morgan = require('morgan');
-// const cors = require('cors');
-
-// // Middleware
-// app.use(cors());
-// app.use(morgan('dev'));
-// app.use(express.json());
-
-// // Router
-// const infoRouter = require('./router');
-// app.use('/info', infoRouter);
-
-// // listen port
-// const PORT = process.env.PORT || 5013;
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
-
-// // Db Connection
-// mongoose.connect('mongodb://127.0.0.1:27017/mongoose', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.once('open', () => {
-//   console.log('Database connection successfully');
-// });
-
-// db.on('error', (err) => {
-//   console.error('Database connection error:', err);
-// });
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -43,7 +7,7 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: process.env.APPLICATION_URL,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  methods: "GET,HEAD,PUT,PATCH",
 };
 
 // Middleware
